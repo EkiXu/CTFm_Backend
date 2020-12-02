3e05d8147b7c7d131f9b21266a7ce39efdf5a6b2
Eki <eki@ieki.xyz>
2020-12-02 14:05:32 +0800

    🐞fix(setting): optimize

--- CTFm_backend/settings.py
+++ CTFm_backend/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ')*w9k#q#9gu7)uj=a2jauu@!ll8yn-s2%gbxu%t69m#n2%l!hu'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = "*"
 
